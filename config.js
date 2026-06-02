5b71154e3ee38663f92fa29985ad79ab854303a5
matchaelmo <estarrji@icloud.com>
2026-06-02 16:13:37 -0400

    Build Farout Mapbox starter

---
+++ config.js
@@ -0,0 +1,4 @@
+// Step 1 for beginners: paste your free Mapbox public access token here.
+// It will look like: pk.eyJ1IjoieW91ci1uYW1lIiwiYSI6...
+// Get one at https://account.mapbox.com/access-tokens/
+window.FAROUT_MAPBOX_TOKEN = 'PASTE_YOUR_MAPBOX_PUBLIC_TOKEN_HERE';
